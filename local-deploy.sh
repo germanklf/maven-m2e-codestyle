71a49e359f97911a7a9659379f9b167ca9055295
German Kondolf <german.kondolf@gmail.com>
2012-04-05 18:29:38 -0300

    Added local repo deploy script.

---
+++ local-deploy.sh
@@ -0,0 +1,2 @@
+mvn clean source:jar javadoc:jar deploy -DaltDeploymentRepository=internal::default::http://tilcara:8080/nexus/content/repositories/releases/
+# mvn clean deploy -DaltDeploymentRepository=internal::default::http://tilcara:8080/nexus/content/repositories/releases/
